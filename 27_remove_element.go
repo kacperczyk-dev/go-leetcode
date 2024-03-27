20b41c8505957738d0f8a1db0e9eb99595835074
dkacperzyk <dawid.kacperzyk@infor.com>
2024-03-27 23:00:47 +0100

    Add best solutions from leetcode as a comment below my code

--- 27_remove_element.go
+++ 27_remove_element.go
@@ -25,3 +25,18 @@ func removeElement(nums []int, val int) int {
     }
     return valCnt
 }
+
+
+/*
+// Best solution. Go through each element and if it is not the one we do not want assign it to the current index
+func removeElement(nums []int, val int) int {
+    i := 0
+    for _, num := range nums {
+        if num != val {
+            nums[i] = num
+            i++
+        }
+    }
+    return i
+}
+*/
--- 88_merge_sorted_array.go
+++ 88_merge_sorted_array.go
@@ -35,3 +35,19 @@ func merge(nums1 []int, m int, nums2 []int, n int)  {
     }
     copy(nums1[:], nums)
 }
+
+
+/*
+    // Best solution, iterate backwards and add higher of the two numbers at the end of nums1
+    func merge(nums1 []int, m int, nums2 []int, n int)  {
+    for n != 0 {
+        if m != 0 && nums1[m-1] > nums2[n-1] {
+            nums1[m+n-1] = nums1[m-1]
+            m--
+        } else {
+            nums1[m+n-1] = nums2[n-1]
+            n--
+        }
+    }
+}
+*/
