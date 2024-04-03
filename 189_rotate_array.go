b02cda14721a6b634176394c2c9891c3d01672ec
dkacperzyk <dawid.kacperzyk@infor.com>
2024-04-03 18:24:22 +0200

    189 rotate array

---
+++ 189_rotate_array.go
@@ -0,0 +1,30 @@
+package main
+
+// https://leetcode.com/problems/rotate-array/description/
+
+// Original solution
+// func rotate(nums []int, k int)  {
+//     l := len(nums)
+//     kk := k % l // kk is equal to the rotation without fullcircles
+//     if kk == 0 { // if kk = 0 we do only full circle(s)
+//         return
+//     }
+//     n := make([]int, l, l)
+//     copy(n[:kk], nums[l-kk:l])
+//     copy(n[kk:], nums[:l-kk])
+//     copy(nums, n)
+// }
+
+
+// Own solution: in-place, space = O(1)
+func rotate(nums []int, k int)  {
+    l := len(nums)
+    kk := k % l // kk is equal to the rotation without fullcircles
+    if kk == 0 { // if kk = 0 we do only full circle(s)
+        return
+    }
+    n := make([]int, k, k)
+    copy(n, nums[l-kk:l])
+    copy(nums[kk:], nums[:l-kk])
+    copy(nums[:kk], n)
+}
---
+++ 189_rotate_array_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "testing"
+    "reflect"
+)
+
+func TestRotateArray(t *testing.T) {
+    type In struct {
+        arr []int
+        k int
+    }
+	cases := []struct {
+        in In
+		want []int
+    }{
+        {In{[]int{1, 2, 3, 4, 5, 6, 7}, 3}, []int{5, 6, 7, 1, 2, 3, 4}},
+        {In{[]int{1, 2, 3, 4, 5, 6, 7}, 7}, []int{1, 2, 3, 4, 5, 6, 7}},
+        {In{[]int{1, 2, 3, 4, 5, 6, 7}, 49}, []int{1, 2, 3, 4, 5, 6, 7}},
+        {In{[]int{1}, 35}, []int{1}},
+    }
+    for _, c := range cases {
+        orig := make([]int, len(c.in.arr), len(c.in.arr))
+        copy(orig, c.in.arr)
+		rotate(c.in.arr, c.in.k)
+        got := c.in.arr
+        if !reflect.DeepEqual(got, c.want) {
+            t.Errorf("merge(%v, %v) == %v, want %v", c.in.arr, c.in.k, got, c.want)
+        }
+    }
+}
