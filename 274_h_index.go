81d047e017b2edb3b12906558e97ab1dff13c62c
dkacperzyk <dawid.kacperzyk@infor.com>
2024-04-06 17:25:47 +0200

    274. H-Index

---
+++ 274_h_index.go
@@ -0,0 +1,30 @@
+package main
+
+func hIndex(citations []int) int {
+	if len(citations) == 1 && citations[0] > 0 {
+		return 1
+	}
+	max := 0
+	m := make(map[int]int)
+	for _, v := range citations {
+		if v > max {
+			max = v
+		}
+		m[v] += 1
+	}
+	sum := 0
+	for c := max; c > 0; c-- {
+		articles, ok := m[c]
+		if !ok {
+			continue
+		}
+		if articles+sum <= c {
+			sum += articles
+		} else {
+			if sum < c {
+				sum = c
+			}
+		}
+	}
+	return sum
+}
---
+++ 274_h_index_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestHIndex(t *testing.T) {
+	cases := []struct {
+		in   []int
+		want int
+	}{
+		{[]int{3, 0, 6, 1, 5}, 3},
+		{[]int{1, 3, 1}, 1},
+		{[]int{5}, 1},
+	}
+	for _, c := range cases {
+		got := hIndex(c.in)
+		if got != c.want {
+			t.Errorf("majorityElement(%v) == %v, want %v", c.in, got, c.want)
+		}
+	}
+}
