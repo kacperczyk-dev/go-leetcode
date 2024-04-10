1d6483b9950596f95f4f48bbdb9f3f94e1c09ba1
dkacperzyk <dawid.kacperzyk@infor.com>
2024-04-10 23:42:57 +0200

    Added better solution and link to problem

--- 134_gas_station.go
+++ 134_gas_station.go
@@ -1,5 +1,6 @@
 package main
 
+// https://leetcode.com/problems/gas-station/description/
 func canCompleteCircuit(gas []int, cost []int) int {
 	gasAvailable := 0
 	gasNeeded := 0
@@ -47,3 +48,34 @@ func canCompleteCircuit(gas []int, cost []int) int {
 	}
 	return r
 }
+
+/* Simpler solution, also done myself
+func canCompleteCircuit(gas []int, cost []int) int {
+	startingAt := 0
+	currentGas := 0
+    counter := 0
+	i := 0
+	for {
+        if counter == len(gas) {
+            break
+        }
+		if i >= len(gas) {
+			i = 0
+		}
+		if currentGas+gas[i] < cost[i] {
+			if i < startingAt {
+                startingAt = -1
+				break
+			}
+            startingAt = i + 1
+			currentGas = 0
+            counter = 0
+		} else {
+			currentGas += gas[i] - cost[i]
+            counter++
+		}
+        i++
+	}
+	return startingAt
+}
+*/
